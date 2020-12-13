a40ff297c41d69ed005a1d9e36255aefcc48af71
sonal <sonalkarle01@gmail.com>
2020-12-14 01:13:33 +0530

    Validate password with at least one number

--- Userregi.sh
+++ Userregi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
-read -p "Enter password with number  " password
+read -p "Enter password with at least one number  " password
 pattern=[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]*
 if [[ $password =~  $pattern ]]
 then
-        echo $password****
+        echo "Password = "$password****
 else
         echo "Invalid"
 fi
