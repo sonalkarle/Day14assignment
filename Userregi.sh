eb25f0cfea5c2efb3a6bec8274b45839453d5206
sonal <sonalkarle01@gmail.com>
2020-12-13 23:58:05 +0530

    Enter user last name

--- Userregi.sh
+++ Userregi.sh
@@ -1,2 +1,11 @@
 echo "Welcome"
+#!/bin/bash -x
+read -p "Enter word to validate pattern " Lastname
+pattern="^[A-Z]{3,}$"
+if [[ $Lastname =~  $pattern ]]
+then
+        echo "valid"
+else
+        echo "Invalid"
+fi
 
