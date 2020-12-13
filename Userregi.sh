e8b10023ce72b8ac5fadcdf10ad7f30436d1e06e
sonal <sonalkarle01@gmail.com>
2020-12-14 00:36:52 +0530

    Validate phoneno

--- Userregi.sh
+++ Userregi.sh
@@ -1,2 +1,11 @@
-echo "Welcome"
+#!/bin/bash -x
+read -p "Enter phone number  " phoneno
+pattern="^[0-9][1-9]" "[0-9]{10}"
+if [[ $phoneno =~  $pattern ]]
+then
+        echo $phoneno
+else
+        echo "Invalid"
+fi
+
 
