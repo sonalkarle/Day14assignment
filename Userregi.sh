a4e09c34adfe104e6eb999848065ebe1e983bd60
sonal <sonalkarle01@gmail.com>
2020-12-14 00:23:23 +0530

    User validate email ID

--- Userregi.sh
+++ Userregi.sh
@@ -1,2 +1,10 @@
-echo "Welcome"
+#!/bin/bash -x
+read -p "Enter word to validate email pattern " EmailID
+pattern="^[a-zA-z]+([._][a-z0-9]+)?@[a-z]+.[a-z]+([.][a-z]+)$"
+if [[ $EmailID =~  $pattern ]]
+then
+        echo $EmailID
+else
+        echo "Invalid"
+fi
 
