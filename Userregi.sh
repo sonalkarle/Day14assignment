4e29bea0a0ef823369c727c9989cc0af043eadcb
sonal <sonalkarle01@gmail.com>
2020-12-31 15:47:17 +0530

    Adding rule 1 for password

--- Userregi.sh
+++ Userregi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+##Create predefined rule-1 for password
+##Taking input from user
 read -p "Enter Password  " Password
+##Create pattern for password for rule-1
 passwordlength=[\$.#@a-zA-Z0-9]{8}[a-zA-Z]*
+##Computation
 if [[ $Password =~  $passwodlength ]]
 then
         echo "password=" $Password****
