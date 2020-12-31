d6c49eb8b7e6a8537d014420e264dcee180a03c3
sonal <sonalkarle01@gmail.com>
2020-12-31 15:26:27 +0530

    Check valid mobile format or not

--- Userregi.sh
+++ Userregi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+##Check valid mobile number in format
+##Taking input from User
 read -p "Enter phone number  " phoneno
+##creates pattern for valid mobile number format
 pattern="^[0-9][1-9]" "[0-9]{10}"
+##Computation
 if [[ $phoneno =~  $pattern ]]
 then
         echo $phoneno
