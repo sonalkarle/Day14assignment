cd71db8123c9f69c753b2eabb8ed557fe0e24cd7
sonal <sonalkarle01@gmail.com>
2020-12-31 16:26:40 +0530

    Rule-4 for generating password with one special character

--- Userregi.sh
+++ Userregi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
+##Creating password with at least special character
+##Taking input from user
 read -p "Enter password at least one character " password
+##Generating pattern for password
 pattern="[a-zA-Z0-9]*[#|\$|\@|\!]+[a-zA-Z0-9]*"
-if [[ $password =~  $pattern ]]
+##Computation
+if [[ $password =~ $pattern ]]
 then
         echo "Password = " $password******
 
