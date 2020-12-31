d7b4762f9bc1d1276869874db6d60912448e1227
sonal <sonalkarle01@gmail.com>
2020-12-31 15:21:09 +0530

    Check last name is valid or not

--- Userregi.sh
+++ Userregi.sh
@@ -1,7 +1,10 @@
-echo "Welcome"
 #!/bin/bash -x
+##Check last name is valid or not
+##Take input from user
 read -p "Enter last name " Lastname
+##Create pattern for last name
 pattern="^[A-Z]{3,}$"
+##Computation
 if [[ $Lastname =~  $pattern ]]
 then
         echo $Lastname
