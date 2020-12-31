5dd8793d6993736f57db9c13a1e09d6fc5de12e4
sonal <sonalkarle01@gmail.com>
2020-12-31 16:22:46 +0530

    Rule-3 for at least 1 number in password

--- Userregi.sh
+++ Userregi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+##Create password rule-3 at least one number in password
+##Taking input from User
 read -p "Enter password with at least one number  " password
+##Generating pattern for password
 pattern=[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]*
+##Computation
 if [[ $password =~  $pattern ]]
 then
         echo "Password = "$password****
