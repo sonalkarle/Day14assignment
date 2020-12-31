c11733a90b5c6a4d3d87994a18942c8b37ee2068
sonal <sonalkarle01@gmail.com>
2020-12-31 16:31:12 +0530

    Create email pattern to clear all sample

--- Userregi.sh
+++ Userregi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+##Create sample email ID
+##Taking input from user
 read -p "Enter word to validate email pattern " EmailID
+##Generating pattern for email ID
 pattern='^[a-c]{3}([.|-|+]*[0|1]+){0,3}@[a-z0-9]+.[a-z]+([.][a-z]+)?$'
+##Computation
 if [[ $EmailID =~  $pattern ]]
 then
         echo $EmailID
