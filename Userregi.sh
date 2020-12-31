aaf807a37e6aa4bc154b300ce5074a6939f41d6f
sonal <sonalkarle01@gmail.com>
2020-12-31 15:16:31 +0530

    Check enter first name valid or not

--- Userregi.sh
+++ Userregi.sh
@@ -1,76 +1,13 @@
 #!/bin/bash -x
-echo "Welcome"
-
-<<<<<<< HEAD
+##Check enter first name is valid or not
+##Take input from user
 read -p "Enter First name " Firstname
+##Create pattern for valid input
 pattern="^[A-Z]{3,}$"
+##Computation
 if [[ $Firstname =~  $pattern ]]
 then
         echo $Firstname
-=======
-read -p "Enter word to validate pattern " Lastname
-pattern="^[A-Z]{3,}$"
-if [[ $Lastname =~  $pattern ]]
-then
-        echo $Lastname
-
->>>>>>> UC2
-else
-        echo "Invalid"
-fi
->>>>>>> UC3
-read -p "Enter word to validate email pattern " EmailID
-pattern="^[a-zA-z]+([._][a-z0-9]+)?@[a-z]+.[a-z]+([.][a-z]+)$"
-if [[ $EmailID =~  $pattern ]]
-then
-        echo $EmailID
-else
-        echo "Invalid"
-fi
->>>>>>> UC4
-
-read -p "Enter phone number  " phoneno
-pattern="^[0-9][1-9]" "[0-9]{10}"
-if [[ $phoneno =~  $pattern ]]
-then
-        echo $phoneno
-else
-        echo "Invalid"
-fi
->>>>>>>> UC5
-
-read -p "Enter Password  " Password
-passwordlength=[\$.#@a-zA-Z0-9]{8}[a-zA-Z]*
-if [[ $Password =~  $passwodlength ]]
-then
-        echo "password=" $Password****
-else
-       
- echo "Invalid"
-fi
-read -p "Enter Password with one upper case  " password
-PasswordUpper=[a-zA-Z]*[A-Z]+[a-zA-Z]*
-if [[ $password =~  $PasswordUpper ]]
-then
-        echo "Password=" $password****
 else
 	echo "Invalid"
 fi
-read -p "Enter password with at least one number  " password
-pattern=[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]*
-if [[ $password =~  $pattern ]]
-then
-        echo "Password = "$password****
-else
-        echo "Invalid"
-fi
-read -p "Enter password at least one character " password
-pattern="[a-zA-Z0-9]*[#|\$|\@|\!]+[a-zA-Z0-9]*"
-if [[ $password =~  $pattern ]]
-then
-        echo "Password = " $password******
-
-else
-        echo "Invalid"
-fi
-
