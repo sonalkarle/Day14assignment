98d268f77db22b1e40efc9bdce2cf8fdb098ffea
sonal <sonalkarle01@gmail.com>
2020-12-31 16:16:53 +0530

    Rule-2 at least one upper case in password

--- Userregi.sh
+++ Userregi.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
+##Creating Rule-2 at least one Upper case
+##Taking input from user
 read -p "Enter Password with one upper case  " password
+##Create pattern for Rule-2 at least one Upper case
 Passwordupper=[a-zA-Z]*[A-Z]+[a-zA-Z]*
+##Computation
 if [[ $password =~  $Passwordupper ]]
 then
         echo "Password=" $password****
